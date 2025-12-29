d3cf8feaec3609db3a38038bef2d2601ad37e9a1
Yoepee <kdy2975@naver.com>
2025-12-29 17:34:43 +0900

    [init] backend 설정 변경

--- backend/src/main/kotlin/com/blog/BackendApplication.kt
+++ backend/src/main/kotlin/com/blog/BackendApplication.kt
@@ -1,4 +1,4 @@
-package com.backend
+package com.blog
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
--- backend/src/test/kotlin/com/blog/BackendApplicationTests.kt
+++ backend/src/test/kotlin/com/blog/BackendApplicationTests.kt
@@ -1,4 +1,4 @@
-package com.backend
+package com.blog
 
 import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
