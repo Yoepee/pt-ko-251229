b506d7cba5e65e4c93bb98ce4af71cf85fc10594
Yoepee <kdy2975@naver.com>
2026-01-01 00:48:22 +0900

    [feat] 중복 회원가입 문구 변경

--- backend/src/main/kotlin/com/blog/global/exception/ErrorCode.kt
+++ backend/src/main/kotlin/com/blog/global/exception/ErrorCode.kt
@@ -11,7 +11,7 @@ enum class ErrorCode(
     CONFLICT(409, "이미 존재합니다."),
     INTERNAL_ERROR(500, "서버 오류가 발생했습니다."),
 
-    USERNAME_DUPLICATED(409, "이미 존재하는 username 입니다."),
+    USERNAME_DUPLICATED(409, "이미 존재하는 계정 입니다."),
     USER_NOT_FOUND(404, "사용자를 찾을 수 없습니다."),
     LOGIN_FAILED(401, "아이디 또는 비밀번호가 올바르지 않습니다."),
 
