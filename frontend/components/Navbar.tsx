dc2b2caba3bf12d7157308edb34b648c9a198f8a
Yoepee <kdy2975@naver.com>
2026-01-12 17:10:08 +0900

    [fix] lint 에러 수정

--- frontend/components/Navbar.tsx
+++ frontend/components/Navbar.tsx
@@ -11,7 +11,7 @@ import { usePathname } from 'next/navigation';
 export function Navbar() {
   const pathname = usePathname();
   
-  const { data: categories } = useQuery({
+  useQuery({
     queryKey: queryKeys.categories.all.queryKey,
     queryFn: categoryApi.getAll,
   });
