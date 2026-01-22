708144d5efbec570a1537a728447f007f6765beb
Yoepee <kdy2975@naver.com>
2026-01-22 15:23:33 +0900

    [fix] test환경에서는 적용되지 않도록 수정

--- backend/src/main/kotlin/com/blog/global/quartz/QuartzConfig.kt
+++ backend/src/main/kotlin/com/blog/global/quartz/QuartzConfig.kt
@@ -3,11 +3,17 @@ package com.blog.global.quartz
 import org.quartz.spi.JobFactory
 import org.quartz.spi.TriggerFiredBundle
 import org.springframework.beans.factory.config.AutowireCapableBeanFactory
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.scheduling.quartz.SpringBeanJobFactory
 
 @Configuration
+@ConditionalOnProperty(
+    prefix = "spring.quartz",
+    name = ["job-store-type"],
+    havingValue = "jdbc"
+)
 class QuartzConfig {
 
     @Bean
